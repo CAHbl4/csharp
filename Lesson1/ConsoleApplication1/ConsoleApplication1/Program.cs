f28110a6c851b90aecd9bd230ba2bf002c68576b
a.matuzov@gmail.com <a.matuzov@gmail.com>
2016-07-20 11:53:35 +0200

    Lesson 1

--- Lesson1/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Lesson1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            try
+            {
+                double a = ReadDouble("Введите значение a: ");
+                double x1 = ReadDouble("Введите начальное значение x: ");
+                double x2 = ReadDouble("Введите конечное значение x: ");
+                double step = ReadDouble("Введите шаг x: ");
+                for (double i = x1; i <= x2; i+=step)
+                {
+                    Console.WriteLine("{0,5:F3}\t{1,5:F3}",i,Func1(a,i));
+                }
+
+                double z = ReadDouble("Введите значение z: ");
+                double x = ReadDouble("Введите значение x: ");
+                Console.WriteLine("Результат работы Func2: " + Func2(x,z));
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+
+        private static double Func1(double a, double x)
+        {
+            return (x + Math.Pow(a, 2))/a*1.4 - x;
+        }
+
+        private static double Func2(double x, double z)
+        {
+            if (z < 0 || z > -5)
+            {
+                return x*x + z;
+            }
+            if (z <= -5)
+            {
+                return z*2.5;
+            }
+            return (x*x*x + 1.3)/z;
+        }
+
+        private static double ReadDouble(string s)
+        {
+                Console.Write(s);
+                return double.Parse(Console.ReadLine());
         }
     }
 }
