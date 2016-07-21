8a3a9092e4bb880e48702dfa489bc630136a71d8
a.matuzov@gmail.com <a.matuzov@gmail.com>
2016-07-21 12:07:24 +0200

    Lesson2

---
+++ Lesson2/Lesson2/Lesson2/TestClass1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson2
+{
+    public class TestClass1
+    {
+        public const int a = 1;
+        public static readonly int b;
+
+        public TestClass1(int b)
+        {
+            
+        }
+    }
+}
