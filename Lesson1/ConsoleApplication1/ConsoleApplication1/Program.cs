de73239cea45bca876a3d832c74bfc1c9de6103b
a.matuzov@gmail.com <a.matuzov@gmail.com>
2016-07-21 09:16:01 +0200

    Lesson1

--- Lesson1/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Lesson1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApplication1
 {
@@ -8,18 +9,23 @@ namespace ConsoleApplication1
         {
             try
             {
-                double a = ReadDouble("Введите значение a: ");
-                double x1 = ReadDouble("Введите начальное значение x: ");
-                double x2 = ReadDouble("Введите конечное значение x: ");
-                double step = ReadDouble("Введите шаг x: ");
-                for (double i = x1; i <= x2; i+=step)
-                {
-                    Console.WriteLine("{0,5:F3}\t{1,5:F3}",i,Func1(a,i));
-                }
+                //double a = ReadDouble("Введите значение a: ");
+                //double x1 = ReadDouble("Введите начальное значение x: ");
+                //double x2 = ReadDouble("Введите конечное значение x: ");
+                //double step = ReadDouble("Введите шаг x: ");
+                //for (double i = x1; i <= x2; i+=step)
+                //{
+                //    Console.WriteLine("{0,5:F3}\t{1,5:F3}",i,Func1(a,i));
+                //}
 
-                double z = ReadDouble("Введите значение z: ");
-                double x = ReadDouble("Введите значение x: ");
-                Console.WriteLine("Результат работы Func2: " + Func2(x,z));
+                //double z = ReadDouble("Введите значение z: ");
+                //double x = ReadDouble("Введите значение x: ");
+                //Console.WriteLine("Результат работы Func2: " + Func2(x,z));
+
+
+                int A = ReadInt("Введите А: ");
+                int B = ReadInt("Введите B: ");
+                Func3(A,B);
 
 
             }
@@ -48,10 +54,29 @@ namespace ConsoleApplication1
             return (x*x*x + 1.3)/z;
         }
 
+        private static void Func3(int A, int B)
+        {
+            for (; A <= B; A++)
+            {
+                for (int j = 0; j < A; j++)
+                {
+                    Console.Write("{0,3:D}",A);
+                }
+                Console.WriteLine();
+                
+            }
+        }
+
         private static double ReadDouble(string s)
         {
                 Console.Write(s);
                 return double.Parse(Console.ReadLine());
+        }
+
+        private static int ReadInt(string s)
+        {
+            Console.Write(s);
+            return int.Parse(Console.ReadLine());
         }
     }
 }
